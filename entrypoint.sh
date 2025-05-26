d298d2d125b8f5d9e7d33d638e34e80627f42c52
sureshchouksey8 <sureshchouksey8@gmail.com>
2025-05-26 20:28:44 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Pass all CLI args ($@) into news_pipeline.py
+set -eux
 python news_pipeline.py "$@"
-# If an editorial was written, print it to the log
 [ -f editorial.txt ] && cat editorial.txt
